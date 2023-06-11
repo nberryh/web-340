3aadb904123d255c8ed3053527abadb95b2221f4
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-06-11 15:00:47 -0500

    updates

--- week-3/index.js
+++ week-3/index.js
@@ -7,24 +7,26 @@ const allTeams = getTeams();
 console.log('-- DISPLAYING TEAMS: --');
 allTeams.forEach(team => {
     console.log(displayTeam(team));
-    console.log('-----------------------');
+    console.log();
 });
+
 
 //Displaying single team objects
 const team1 = getTeam('Alabama');
 const team2 = getTeam('Georgia');
 
 console.log('-- DISPLAYING A SINGLE TEAM: --');
-console.log('Team 1:');
 if (team1) {
     console.log(displayTeam(team1));
 } else {
     console.log('Team not found.');
 }
+console.log();
+
 console.log('-- DISPLAYING A SINGLE TEAM: --');
-console.log('Team 3:');
 if (team2) {
     console.log(displayTeam(team2));
 } else {
     console.log('Team not found.');
 }
+console.log();
